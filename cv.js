dc86a2f8033dac11cc7b80080cf0fac142c6825e
feha-tlmcn <Tlemcanimaymouna3@gmail.com>
2025-05-05 15:14:38 +0200

    modif

--- cv.js
+++ cv.js
@@ -24,4 +24,58 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+
+const faders = document.querySelectorAll
+  ('.fade-in');
+
+const appearOptions = {
+  threshold : 0,
+  rootMargin: "0px 0px -400px 0px"
+}; 
+const appearOnScroll = new IntersectionObserver
+  (function(
+    entries, 
+    appearOnScroll
+  ){
+      entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+          return;
+        } else {
+          entry.target.classList.add("appear");
+          appearOnScroll.unobserve(entry.target);
+        }
+      });
+    }, 
+    appearOptions);
+      faders.forEach(fader => {
+      appearOnScroll.observe(fader);
+})
   
+
+
+const buttons = document.querySelectorAll
+('.fade-in-btn');
+
+const buttonsOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -400px 0px" 
+};
+
+const buttonsObserver = new IntersectionObserver
+  (function(
+    entries, 
+    observer
+  ){
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, 
+  buttonsOptions);
+    buttons.forEach(button => {
+    buttonsObserver.observe(button);
+});
--- profil.js
+++ profil.js
@@ -1,3 +1,31 @@
 $('.button').tilt({
     speed: 1000, 
     scale: 1.1}); 
+
+
+
+const faders = document.querySelectorAll
+('.fade-in');
+  
+const appearOptions = {
+    threshold : 0,
+    rootMargin: "0px 0px -400px 0px"
+}; 
+const appearOnScroll = new IntersectionObserver
+    (function(
+      entries, 
+      appearOnScroll
+){
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+         return;
+        } else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+});
+    }, 
+    appearOptions);
+    faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
